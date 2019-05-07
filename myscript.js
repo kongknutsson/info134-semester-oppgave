6f04ea522531b5c1dd02e85d7abf595df751c925
karolinis <48238451+karolinis@users.noreply.github.com>
2019-05-07 11:12:32 +0200

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -88,7 +88,6 @@ function addToOversikt(befolkning ){
   }
 }
 
-
 function detaljerSearcher() {
   // Declare variables
   var input, filter, table, tr, td, i, txtValue;
@@ -99,7 +98,7 @@ function detaljerSearcher() {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+    td = tr[i].getElementsByTagName("td")[1];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
