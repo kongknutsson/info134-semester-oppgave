24b4d5f430b2be3def602a042a5342bab90f1998
karolinis <48238451+karolinis@users.noreply.github.com>
2019-05-07 11:09:00 +0200

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,11 +1,14 @@
 function openNav(){
+  if (window.matchMedia("(max-width: 960px)").matches) {
+    document.getElementById('sidenav').style.width = "180px";
+  } else {
   document.getElementById("sidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  }
 }
 function closeNav(){
   document.getElementById("sidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
 }
+
 function oversikt(){
   document.getElementById("Introduksjon").style.display = "none";
   document.getElementById("Detaljer").style.display = "none";
@@ -82,5 +85,28 @@ function addToOversikt(befolkning ){
     var menn = befolkning.elementer[key].Menn[2018];
     var kvinner = befolkning.elementer[key].Kvinner[2018];
     cell3.innerHTML = parseInt(menn) + parseInt(kvinner);
+  }
+}
+
+
+function detaljerSearcher() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("detaljer_input");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("detaljer_table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
   }
 }
