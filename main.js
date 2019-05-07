84f242a12620548aac83ed7841fc56a3fc65e051
kongknutsson <adrian@knutsson.no>
2019-05-07 09:25:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,145 +1,91 @@
-function openPage(pageName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
-  // Show the specific tab content
-  document.getElementById(pageName).style.display = "block";
-  // Add the specific color to the button used to open the tab content
-  elmnt.style.backgroundColor = color;
+function openNav(){
+  document.getElementById("sidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 }
-// Get the element with id="defaultOpen" and click on it
+function closeNav(){
+  document.getElementById("sidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+function oversikt(){
+  document.getElementById("Introduksjon").style.display = "none";
+  document.getElementById("Detaljer").style.display = "none";
+  document.getElementById("Sammenligning").style.display = "none";
+  document.getElementById("Oversikt").style.display = "block";
+}
+function introduksjon(){
+  document.getElementById("Oversikt").style.display = "none";
+  document.getElementById("Detaljer").style.display = "none";
+  document.getElementById("Sammenligning").style.display = "none";
+  document.getElementById("Introduksjon").style.display = "block";
+}
+function detaljer(){
+   document.getElementById("Introduksjon").style.display = "none";
+   document.getElementById("Oversikt").style.display = "none";
+   document.getElementById("Sammenligning").style.display = "none";
+   document.getElementById("Detaljer").style.display = "block";
+}
+function sammenligning(){
+  document.getElementById("Introduksjon").style.display = "none";
+  document.getElementById("Oversikt").style.display = "none";
+  document.getElementById("Detaljer").style.display = "none";
+  document.getElementById("Sammenligning").style.display = "block";
+}
 
-// parsing OBS, må endre på variabelnavn!
-window.onload = function() {
-    document.getElementById("Save").onclick = function gud2() {
-        var Url = "http://wildboy.uib.no/~tpe056/folk/104857.json";
-        var xhr = new XMLHttpRequest();
-        var kommunenr = (document.forms["myForm"]["id"].value);
-        xhr.open('GET', Url, true);
-        xhr.send();
-        xhr.onreadystatechange = processRequest;
+function Befolkning(url){
+  this.skjema = null;
+  this.load = loader(url);
 
-        function processRequest(e) {
-            if (this.readyState === 4 && this.status === 200) {
-                var response1 = JSON.parse(xhr.responseText);
-                var data = Object.entries(response1.elementer);
-                var i = 0;
-                for (var i; i < data.length; i++) {
-                    if (parseInt(data[i][1].kommunenummer) == kommunenr) {
-                        mennData = data[i][1].Menn
-                        kvinneData = data[i][1].Kvinner
-                        console.log("success");
-                        document.getElementById("KommuneNavn").innerHTML = data[i][0];
-                        document.getElementById("KommuneNummer").innerHTML = kommunenr;
-                        document.getElementById("menn").innerHTML = mennData["2018"];
-                        document.getElementById("kvinner").innerHTML = kvinneData["2018"];
-                        break;
-                    }
-                }
-            }
-        }
+  function loader(link){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", link);
+    xhr.onreadystatechange = function(){loader_request();}
 
-        var url1 = "http://wildboy.uib.no/~tpe056/folk/100145.json";
-        var xhr1 = new XMLHttpRequest();
-        console.log(kommunenr);
-        xhr1.open('GET', url1, true);
-        xhr1.send();
-        xhr1.onreadystatechange = processRequest1;
+    function loader_request(){
+      console.log("state:", xhr.readyState);
+      console.log("status:", xhr.status);
+      if (xhr.readyState === 4 && xhr.status === 200){
+        var skjema = JSON.parse(xhr.responseText);
+        addToOversikt(skjema);
+        return skjema;
+      }
+    }
+    xhr.send();
+    return loader_request;
+  }
 
-        function processRequest1(e) {
-            if (this.readyState === 4 && this.status === 200) {
-                var response2 = JSON.parse(xhr1.responseText);
-                var data1 = Object.entries(response2.elementer);
-                var i = 0;
-                for (var i; i < data1.length; i++) {
-                    if (parseInt(data1[i][1].kommunenummer) == kommunenr) {
-                        console.log("success1");
-                        alleData1 = data1[i][1];
-                        mennData1 = alleData1.Menn;
-                        kvinneData1 = alleData1.Kvinner;
-                        beggeKjønn1 = alleData1['Begge kjønn'];
-                        document.getElementById("menniarbeid1").innerHTML = mennData1["2018"];
-                        document.getElementById("kvinneriarbeid1").innerHTML = kvinneData1["2018"];
-                        document.getElementById("beggekjønniarbeid1").innerHTML = beggeKjønn1["2018"];
-                    }
-                }
-            }
-        }
+  this.getNames = function(){
+    this.skjema = "ok";
+  };
+  this.getIDS = function(){};
+  this.getInfo = function (){};
+}
 
-        var url2 = "http://wildboy.uib.no/~tpe056/folk/85432.json";
-        var xhr2 = new XMLHttpRequest();
-        console.log(kommunenr);
-        xhr2.open('GET', url2, true);
-        xhr2.send();
-        xhr2.onreadystatechange = processRequest2;
+var bef = new Befolkning("http://wildboy.uib.no/~tpe056/folk/104857.json");
+bef.load();
 
-        function processRequest2(e) {
-            if (this.readyState === 4 && this.status === 200) {
-                var response3 = JSON.parse(xhr2.responseText);
-                var data2 = Object.entries(response3.elementer);
-                var i = 0;
-                for (var i; i < data2.length; i++) {
-                    if (parseInt(data2[i][1].kommunenummer) == kommunenr) {
-                        console.log("success2");
-                        console.log(data2[i][1]["01"].Menn);
-                        document.getElementById("M01").innerHTML = data2[i][1]["01"].Menn["2017"];
-                        document.getElementById("M02a").innerHTML = data2[i][1]["02a"].Menn["2017"];
-                        document.getElementById("M11").innerHTML = data2[i][1]["11"].Menn["2017"];
-                        document.getElementById("M03a").innerHTML = data2[i][1]["03a"].Menn["2017"];
-                        document.getElementById("M04a").innerHTML = data2[i][1]["03a"].Menn["2017"];
-                        document.getElementById("M09a").innerHTML = data2[i][1]["09a"].Menn["2017"];
-
-                        document.getElementById("K01").innerHTML = data2[i][1]["01"].Kvinner["2017"];
-                        document.getElementById("K02a").innerHTML = data2[i][1]["02a"].Kvinner["2017"];
-                        document.getElementById("K11").innerHTML = data2[i][1]["11"].Kvinner["2017"];
-                        document.getElementById("K03a").innerHTML = data2[i][1]["03a"].Kvinner["2017"];
-                        document.getElementById("K04a").innerHTML = data2[i][1]["04a"].Kvinner["2017"];
-                        document.getElementById("K09a").innerHTML = data2[i][1]["09a"].Kvinner["2017"];
-
-                    }
-                }
-            }
-
-        }
-    };
-
-    document.getElementById("Save3").onclick = function gud() {
-        var Url3 = "http://wildboy.uib.no/~tpe056/folk/104857.json";
-        var xhr3 = new XMLHttpRequest();
-        var kommunenr3 = (document.forms["myForm3"]["id3"].value);
-        xhr3.open('GET', Url3, true);
-        xhr3.send();
-        xhr3.onreadystatechange = processRequest3;
-
-        function processRequest3(e3) {
-            if (this.readyState === 4 && this.status === 200) {
-                var response4 = JSON.parse(xhr3.responseText);
-                var data3 = Object.entries(response4.elementer);
-                var i3 = 0;
-                console.log(data3);
-                console.log(kommunenr3);
-                console.log(data3[i3][1].Menn);
-                for (var i3; i3 < data3.length; i3++) {
-                    if (parseInt(data3[i3][1].kommunenummer) == kommunenr3) {
-                        mennData3 = data3[i3][1].Menn
-                        kvinneData3 = data3[i3][1].Kvinner
-                        console.log("success");
-                        document.getElementById("KommuneNavn3").innerHTML = data3[i3][0];
-                        document.getElementById("KommuneNummer3").innerHTML = kommunenr3;
-                        document.getElementById("menn3").innerHTML = mennData3["2018"];
-                        document.getElementById("kvinner3").innerHTML = kvinneData3["2018"];
-                        break;
-                    }
-                }
-            }
-        }
-    };
-};
+// Henter ut de relevante IDene vi trenger.
+oversikt_table = document.getElementById("oversikt_table");
+oversikt_navn = document.getElementById("oversikt_navn");
+oversikt_nummer = document.getElementById("oversikt_nummer");
+oversikt_befolkning = document.getElementById("oversikt_befolkning");
+// Legger til data i oversikt, med et nedlastet JSON objekt som parameter.
+function addToOversikt(befolkning ){
+  // bruker nøkkelverdiene til å hente ut objektene.
+  for (var key of Object.keys(befolkning.elementer)) {
+    // Lager nye rader og rekker til infoen
+    var row = oversikt_table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.classList.add("info");
+    cell2.classList.add("info");
+    cell3.classList.add("info");
+    // legger inn infoen i rekkene. key er nøkkelverdi, altså kommunenavn.
+    cell1.innerHTML = key;
+    cell2.innerHTML = befolkning.elementer[key].kommunenummer;
+    // Bruker det seneste året, altså 2018.
+    var menn = befolkning.elementer[key].Menn[2018];
+    var kvinner = befolkning.elementer[key].Kvinner[2018];
+    cell3.innerHTML = parseInt(menn) + parseInt(kvinner);
+  }
+}
