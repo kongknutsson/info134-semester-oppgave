2a4fe4f264bd95d83e379401d83c41ce099d7827
kongknutsson <adrian@knutsson.no>
2019-05-08 14:08:30 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -8,7 +8,6 @@ function openNav(){
 function closeNav(){
   document.getElementById("sidenav").style.width = "0";
 }
-
 function oversikt(){
   document.getElementById("Introduksjon").style.display = "none";
   document.getElementById("Detaljer").style.display = "none";
@@ -35,11 +34,11 @@ function sammenligning(){
 }
 
 function my_constructor(url){
-  this.load = loader(url);
+  this.load = loader(url, this);
   this.skjema = this.load;
   this.onload = null;
 
-  function loader(link){
+  function loader(link, myObject){
     var xhr = new XMLHttpRequest();
     xhr.open("GET", link, false);
     xhr.send();
@@ -69,14 +68,34 @@ function my_constructor(url){
       }
     }
   }
+  this.getNameFrom = function(kommunenummer){
+    for (kommune in this.skjema.elementer){
+      if (this.skjema.elementer[kommune].kommunenummer == kommunenummer){
+        return kommune;
+      }
+    }
+  }
 }
 
 oversikt_table = document.getElementById("oversikt_table");
 oversikt_navn = document.getElementById("oversikt_navn");
 oversikt_nummer = document.getElementById("oversikt_nummer");
 oversikt_befolkning = document.getElementById("oversikt_befolkning");
+
+function addToDetaljer(befolkning, sysselsatte, utdanning, input){
+  for (var i = 0; i < befolkning.getIDs().length; i++){
+    console.log("ok");
+    if (input in befolkning.getIDs){
+      bef_info = befolkning.getInfo(input);
+      document.getElementById("output_kommune").innerHTML = befolkning.getNameFrom(input);
+      document.getElementById("output_nummer").innerHTML = input;
+      document.getElementById("output_menn").innerHTML = bef_info.Menn[2018];
+      document.getElementById("output_kvinner").innerHTML = bef_info.Kvinner[2018];
+      sys_info = sysselsatte.getInfo(input);
+    }
+  }
+}
 function addToOversikt(skjema){
-  // bruker nøkkelverdiene til å hente ut objektene.
   for (var key of Object.keys(skjema.elementer)) {
     var row = oversikt_table.insertRow(1);
     var cell1 = row.insertCell(0);
@@ -85,15 +104,14 @@ function addToOversikt(skjema){
     cell1.classList.add("info");
     cell2.classList.add("info");
     cell3.classList.add("info");
-    // legger inn infoen i rekkene. key er nøkkelverdi, altså kommunenavn.
     cell1.innerHTML = key;
     cell2.innerHTML = skjema.elementer[key].kommunenummer;
-    // Bruker det seneste året, altså 2018.
     var menn = skjema.elementer[key].Menn[2018];
     var kvinner = skjema.elementer[key].Kvinner[2018];
     cell3.innerHTML = parseInt(menn) + parseInt(kvinner);
   }
 }
+
 function detaljerSearcher() {
   // Declare variables
   var input, filter, table, tr, td, i, txtValue;
@@ -117,11 +135,18 @@ function detaljerSearcher() {
 }
 
 function main(){
-  var befolkning = new my_constructor("http://wildboy.uib.no/~tpe056/folk/104857.json");
-  var sysselsatte = new my_constructor("http://wildboy.uib.no/~tpe056/folk/100145.json");
-  var utdanning = new my_constructor("http://wildboy.uib.no/~tpe056/folk/85432.json");
+  var befolkning_url = "http://wildboy.uib.no/~tpe056/folk/104857.json";
+  var sysselsatte_url = "http://wildboy.uib.no/~tpe056/folk/100145.json";
+  var utdanning_url =  "http://wildboy.uib.no/~tpe056/folk/85432.json";
+
+  var befolkning = new my_constructor(befolkning_url);
+  var sysselsatte = new my_constructor(sysselsatte_url);
+  var utdanning = new my_constructor(utdanning_url);
   befolkning.load;
   sysselsatte.load;
   utdanning.load;
-}
 
+  addToOversikt(befolkning.skjema);
+  addToDetaljer(befolkning, sysselsatte, utdanning, "0101")
+}
+main();
